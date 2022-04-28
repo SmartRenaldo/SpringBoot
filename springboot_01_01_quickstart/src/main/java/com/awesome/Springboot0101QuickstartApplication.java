c39d635398f05ea4b5e7188a58c72f252d3db9bc
SmartRenaldo <correctgavin@gmail.com>
2022-04-28 21:02:14 +0930

    overview

--- springboot_01_01_quickstart/src/main/java/com/awesome/Springboot0101QuickstartApplication.java
+++ springboot_01_01_quickstart/src/main/java/com/awesome/Springboot0101QuickstartApplication.java
@@ -1,13 +1,17 @@
 package com.awesome;
 
+import com.awesome.controller.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0101QuickstartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        BookController bookController = applicationContext.getBean(BookController.class);
+        System.out.println("bookController = " + bookController);
     }
 
 }
