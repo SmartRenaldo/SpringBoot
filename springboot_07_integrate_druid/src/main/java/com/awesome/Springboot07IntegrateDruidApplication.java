cce1ab19656d47166cab08181691e7d405ec4162
SmartRenaldo <correctgavin@gmail.com>
2022-04-30 00:38:18 +0930

    integrate druid

---
+++ springboot_07_integrate_druid/src/main/java/com/awesome/Springboot07IntegrateDruidApplication.java
@@ -0,0 +1,13 @@
+package com.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot07IntegrateDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot07IntegrateDruidApplication.class, args);
+    }
+
+}
---
+++ springboot_07_integrate_druid/src/test/java/com/awesome/Springboot07IntegrateDruidApplicationTests.java
@@ -0,0 +1,18 @@
+package com.awesome;
+
+import com.awesome.mapper.DepartmentMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07IntegrateDruidApplicationTests {
+    @Autowired
+    private DepartmentMapper departmentMapper;
+
+    @Test
+    void contextLoads() {
+        System.out.println(departmentMapper.getByDepartmentId(10));
+    }
+
+}
