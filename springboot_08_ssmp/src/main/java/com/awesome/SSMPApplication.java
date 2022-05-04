563d11bd736bb48e87846200b098bdf3750cb169
SmartRenaldo <correctgavin@gmail.com>
2022-05-04 13:15:20 +0930

    4-level classification of configuration files

--- springboot_08_ssmp/src/main/java/com/awesome/SSMPApplication.java
+++ springboot_08_ssmp/src/main/java/com/awesome/SSMPApplication.java
@@ -3,11 +3,17 @@ package com.awesome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class SSMPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SSMPApplication.class, args);
+        String[] arg = new String[1];
+        arg[0] = "--server.port=8082";
+        //close the entry of external configuration
+        SpringApplication.run(SSMPApplication.class);
+        //SpringApplication.run(SSMPApplication.class, arg);
     }
 
 }
