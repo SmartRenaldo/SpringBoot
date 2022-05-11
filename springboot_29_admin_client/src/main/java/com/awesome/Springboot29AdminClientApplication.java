f6c6392bd45b8f924be7b20b31469f023311139f
SmartRenaldo <correctgavin@gmail.com>
2022-05-11 23:31:04 +0930

    spring boot admin

---
+++ springboot_29_admin_client/src/main/java/com/awesome/Springboot29AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot29AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot29AdminClientApplication.class, args);
+    }
+
+}
---
+++ springboot_29_admin_client/src/test/java/com/awesome/Springboot29AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot29AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
